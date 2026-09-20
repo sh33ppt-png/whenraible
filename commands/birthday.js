ef7043ec74fa161a78301076af26fc2cd27cde98
sh33ppt-png <sh33p.pt@gmail.com>
2026-09-21 06:57:05 +0700

    Create birthday.js

---
+++ commands/birthday.js
@@ -0,0 +1,28 @@
+module.exports = async function birthdayCommand(message) {
+  const member = message.mentions.members.first();
+
+  if (!member) {
+    return message.reply(
+      "❌ Tag người mà Wannie cần chúc sinh nhật nhaaa!"
+    );
+  }
+
+  const name = member.user.username;
+
+  const birthdayMessage =
+`Happy Birthday, **${name}**! 🎉
+
+Mong rằng bạn sẽ gặp thật nhiều may mắn, công việc thuận lợi cũng như up R99 ầm ầm >^<
+Chúc bạn có một ngày sinh nhật thật vui nhóooo ✨
+
+From Wannie, with love <3
+
+${member}`;
+
+  return message.channel.send({
+    content: birthdayMessage,
+    allowedMentions: {
+      users: [member.id]
+    }
+  });
+};
